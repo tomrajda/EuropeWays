09e1215dbb22bc2805a88b00b5f4c1f443651089
tomrajda <eu.crafciarze@gmail.com>
2024-05-15 18:59:24 +0200

    merged code macek, tomek

--- src/components/FlightSearch.vue
+++ src/components/FlightSearch.vue
@@ -222,102 +222,105 @@ const saveFlightDetails = () => {
 </script>
 
 <template>
-<div class="container mx-auto">
-
-    <h1 class="text-2xl font-semibold mb-6 mt-32" v-if="departDate && !returnDate">Find one way cheapest flight near <b>{{ departDate }}</b></h1>
-    <h1 class="text-2xl font-semibold mb-6 mt-32" v-else-if="returnDate && departDate">Find cheapest flight from <b>{{ departDate }}</b> to <b>{{returnDate}}</b></h1>
-    <h1 class="text-2xl font-semibold mb-6 mt-32" v-else>Let's Trip!</h1>
-
-    <div>Choose option: </div>
-    <label class="inline-flex items-center">
-        <div class="mt-3">
-            <input type="radio" name="flightOption" value="oneWay" class="form-radio h-6 w-6 text-green-500 rounded" v-model="selectedOption">
-            <span class="ml-1 inliml-2 text-gray-700">One way</span>
-            
-            <input type="radio" name="flightOption" value="return" class="ml-8 form-radio h-6 w-6 text-green-500 rounded" v-model="selectedOption">
-            <span class="ml-1 inliml-2 text-gray-700">Return</span>
-        </div>
-    </label>
-
-  <form @submit.prevent="handleSubmit" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-8">
-    <div class="mb-4">
-      <label for="origin" class="block text-sm font-medium text-gray-700 mb-2">From</label>
-      <select id="origin" v-model="origin" class="form-select w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
-        <option value="">Departure</option>
-        <option v-for="airport in airports" :key="airport.iataCode" :value="airport.iataCode">{{ airport.name }}</option>
-      </select>
+  <div class="container mx-auto">
+  
+      <h1 class="text-2xl font-semibold mb-6 mt-32" v-if="departDate && !returnDate">Find one way cheapest flight near <b>{{ departDate }}</b></h1>
+      <h1 class="text-2xl font-semibold mb-6 mt-32" v-else-if="returnDate && departDate">Find cheapest flight from <b>{{ departDate }}</b> to <b>{{returnDate}}</b></h1>
+      <h1 class="text-2xl font-semibold mb-6 mt-32" v-else>Let's Trip!</h1>
+  
+      <div>Choose option: </div>
+      <label class="inline-flex items-center">
+          <div class="mt-3">
+              <input type="radio" name="flightOption" value="oneWay" class="form-radio h-6 w-6 text-green-500 rounded" v-model="selectedOption">
+              <span class="ml-1 inliml-2 text-gray-700">One way</span>
+              
+              <input type="radio" name="flightOption" value="return" class="ml-8 form-radio h-6 w-6 text-green-500 rounded" v-model="selectedOption">
+              <span class="ml-1 inliml-2 text-gray-700">Return</span>
+          </div>
+      </label>
+  
+    <form @submit.prevent="handleSubmit" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-8">
+      <div class="mb-4">
+        <label for="origin" class="block text-sm font-medium text-gray-700 mb-2">From</label>
+        <select id="origin" v-model="origin" class="form-select w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+          <option value="">Departure</option>
+          <option v-for="airport in airports" :key="airport.iataCode" :value="airport.iataCode">{{ airport.name }}</option>
+        </select>
+      </div>
+  
+      <div class="mb-4">
+        <label for="destination" class="block text-sm font-medium text-gray-700 mb-2">To</label>
+        <select id="destination" v-model="destination" :disabled="!origin" class="form-select w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 disabled:bg-gray-300">
+          <option value="">Destination</option>
+          <option v-for="airport in airportsAval" :key="airport.arrivalAirport.code" :value="airport.arrivalAirport.code">{{ airport.arrivalAirport.name }}</option>
+        </select>
+      </div>
+  
+      <div class="mb-4">
+        <label for="departDate" class="block text-sm font-medium text-gray-700 mb-2">Departure Date</label>
+        <input type="date" id="departDate" v-model="departDate" class="form-input w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+      </div>
+  
+  
+  
+      <div v-show="selectedOption == 'return'" class="mb-4">
+        <label for="returnDate" class="block text-sm font-medium text-gray-700 mb-2">Return Date</label>
+        <input type="date" id="returnDate" v-model="returnDate" :min="departDate" :disabled="!departDate" class="form-input w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 disabled:bg-gray-300">
+      </div>
+  
+  
+      <div class="mb-4">
+        <label for="currency" class="block text-sm font-medium text-gray-700 mb-2">Currency</label>
+        <select id="currency" v-model="currency" class="form-select w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+          <option value="EUR">EUR</option>
+          <option value="USD">USD</option>
+          <option value="PLN">PLN</option>
+        </select>
+      </div>
+      <button 
+          type="submit"
+          :disabled="(!origin || !destination || !departDate || (!returnDate && selectedOption === 'return'))"
+          class="w-full py-3 px-6 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50">Search</button>
+    </form>
+  
+    <div v-if="error" class="text-red-500 mb-8">{{ error }}</div>
+  
+    <div v-if="cheapestFlight !== null" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-8">
+      <h2 class="text-lg font-semibold mb-4">Cheapest Flight:</h2>
+      <p>Departure Date: {{ formatReadableDate(cheapestFlight.departureDate) }}</p>
+      <p>Arrival Date: {{ formatReadableDate(cheapestFlight.arrivalDate) }}</p>
+      <p>Price: {{ cheapestFlight.price.value }} {{ cheapestFlight.price.currencySymbol }}</p>
     </div>
-
-    <div class="mb-4">
-      <label for="destination" class="block text-sm font-medium text-gray-700 mb-2">To</label>
-      <select id="destination" v-model="destination" :disabled="!origin" class="form-select w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 disabled:bg-gray-300">
-        <option value="">Destination</option>
-        <option v-for="airport in airportsAval" :key="airport.arrivalAirport.code" :value="airport.arrivalAirport.code">{{ airport.arrivalAirport.name }}</option>
-      </select>
+  
+    <div v-if="cheapestReturnFlight !== null && cheapestFlight !== null" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-8">
+      <h2 class="text-lg font-semibold mb-4">Return:</h2>
+      <p>Return Date: {{ formatReadableDate(cheapestReturnFlight.departureDate) }}</p>
+      <p>Arrival Date: {{ formatReadableDate(cheapestReturnFlight.arrivalDate) }}</p>
+      <p>Price: {{ cheapestReturnFlight.price.value }} {{ cheapestReturnFlight.price.currencySymbol }}</p>
     </div>
-
-    <div class="mb-4">
-      <label for="departDate" class="block text-sm font-medium text-gray-700 mb-2">Departure Date</label>
-      <input type="date" id="departDate" v-model="departDate" class="form-input w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+  
+    <div v-if="cheapestFlight == null && !error && cheapestCallResponse !== null" class="text-gray-700 bg-white shadow-md rounded px-8 py-4 mb-8">
+      <p>No flights found, try different date.</p>
     </div>
-
-
-
-    <div v-show="selectedOption == 'return'" class="mb-4">
-      <label for="returnDate" class="block text-sm font-medium text-gray-700 mb-2">Return Date</label>
-      <input type="date" id="returnDate" v-model="returnDate" :min="departDate" :disabled="!departDate" class="form-input w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 disabled:bg-gray-300">
+  
+  
+    <div v-if="cheapestReturnFlight === null && !error && cheapestReturnCallResponse !== null && selectedOption==='return'" class="text-gray-700 bg-white shadow-md rounded px-8 py-4 mb-8">
+      <p>No return flights found, try different date.</p>
     </div>
-
-
-    <div class="mb-4">
-      <label for="currency" class="block text-sm font-medium text-gray-700 mb-2">Currency</label>
-      <select id="currency" v-model="currency" class="form-select w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
-        <option value="EUR">EUR</option>
-        <option value="USD">USD</option>
-        <option value="PLN">PLN</option>
-      </select>
+    
+    <div>
+      <form @submit.prevent="saveHistory">
+        <button 
+            type="submit" 
+            v-if="cheapestFlight" 
+            @click="saveFlightDetails" 
+            class="w-full py-3 px-6 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50"
+            :disabled="cheapestReturnFlight == null">Save flight</button>
+      </form>
     </div>
-    <button 
-        type="submit"
-        :disabled="(!origin || !destination || !departDate || (!returnDate && selectedOption === 'return'))"
-        class="w-full py-3 px-6 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50">Search</button>
-  </form>
-
-  <div v-if="error" class="text-red-500 mb-8">{{ error }}</div>
-
-  <div v-if="cheapestFlight !== null" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-8">
-    <h2 class="text-lg font-semibold mb-4">Cheapest Flight:</h2>
-    <p>Departure Date: {{ formatReadableDate(cheapestFlight.departureDate) }}</p>
-    <p>Arrival Date: {{ formatReadableDate(cheapestFlight.arrivalDate) }}</p>
-    <p>Price: {{ cheapestFlight.price.value }} {{ cheapestFlight.price.currencySymbol }}</p>
-  </div>
-
-  <div v-if="cheapestReturnFlight !== null && cheapestFlight !== null" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-8">
-    <h2 class="text-lg font-semibold mb-4">Return:</h2>
-    <p>Return Date: {{ formatReadableDate(cheapestReturnFlight.departureDate) }}</p>
-    <p>Arrival Date: {{ formatReadableDate(cheapestReturnFlight.arrivalDate) }}</p>
-    <p>Price: {{ cheapestReturnFlight.price.value }} {{ cheapestReturnFlight.price.currencySymbol }}</p>
-  </div>
-
-  <div v-if="cheapestFlight == null && !error && cheapestCallResponse !== null" class="text-gray-700 bg-white shadow-md rounded px-8 py-4 mb-8">
-    <p>No flights found, try different date.</p>
-  </div>
-
-
-  <div v-if="cheapestReturnFlight == null && !error && cheapestReturnCallResponse !== null" class="text-gray-700 bg-white shadow-md rounded px-8 py-4 mb-8">
-    <p>No return flights found, try different date.</p>
+  
+  
   </div>
   
-  <div>
-    <form @submit.prevent="saveHistory">
-      <button 
-          type="submit" 
-          v-if="cheapestFlight" 
-          @click="saveFlightDetails" 
-          class="w-full py-3 px-6 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50"
-          :disabled="cheapestReturnFlight == null">Save flight</button>
-    </form>
-  </div>
-</div>
-
-</template>
+  
+  </template>
