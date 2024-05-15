2ff0a10a26dd61546763467a27bd2889cfc4ac3e
mjedrzejowski2 <mjedrzejowski00@gmail.com>
2024-05-15 19:49:41 +0200

    Bug fix 1.1

--- src/components/FlightSearch.vue
+++ src/components/FlightSearch.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { ref, onMounted, watch } from 'vue';
 import axios from 'axios';
+import { useAuthStore } from '../stores/auth';
 
+const authStore = useAuthStore();
 const origin = ref('');
 const destination = ref('');
 const departDate = ref('');
@@ -308,18 +310,19 @@ const saveFlightDetails = () => {
       <p>No return flights found, try different date.</p>
     </div>
     
-    <div>
+    <div v-if="authStore.user">
       <form @submit.prevent="saveHistory">
         <button 
             type="submit" 
             v-if="cheapestFlight" 
             @click="saveFlightDetails" 
             class="w-full py-3 px-6 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50"
-            :disabled="cheapestReturnFlight == null">Save flight</button>
+            :disabled="cheapestReturnFlight === null && selectedOption ==='return'">
+            Save {{ selectedOption === 'return' ? 'Return' : 'One-Way' }} Flight
+        </button>
       </form>
     </div>
-  
-  
+
   </div>
   
   
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -108,6 +108,27 @@
                         md:dark:bg-gray-900
                     "
                 >
+                <li>
+                            <router-link :to="{name: 'FlightSearch'}"
+                                class="
+                                    block
+                                    rounded
+                                    py-2
+                                    pr-4
+                                    pl-3
+                                    text-gray-700
+                                    hover:bg-gray-100
+                                    dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white
+                                    md:border-0
+                                    md:p-0
+                                    md:hover:bg-transparent
+                                    md:hover:text-blue-700
+                                    md:dark:hover:bg-transparent
+                                    md:dark:hover:text-white
+                                "
+                                >Flight search</router-link
+                            >
+                        </li>
                     <template v-if="!authStore.user">
                         <li>
                             <router-link :to="{name: 'Login'}"
@@ -174,27 +195,7 @@
                                 >My flights</router-link
                             >
                         </li>
-                        <li>
-                            <router-link :to="{name: 'FlightSearch'}"
-                                class="
-                                    block
-                                    rounded
-                                    py-2
-                                    pr-4
-                                    pl-3
-                                    text-gray-700
-                                    hover:bg-gray-100
-                                    dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white
-                                    md:border-0
-                                    md:p-0
-                                    md:hover:bg-transparent
-                                    md:hover:text-blue-700
-                                    md:dark:hover:bg-transparent
-                                    md:dark:hover:text-white
-                                "
-                                >Flight search</router-link
-                            >
-                        </li>
+                        
                         <button  
                             @click="handleLogout"
                             class="
